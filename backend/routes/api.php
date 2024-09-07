4d7664b6d08e779c5799163a2fd2f7319c650c6b
Alex Gonzalez <galex2003@icloud.com>
2024-09-07 11:39:30 -0400

    Created post controller along with GET & POST

---
+++ backend/routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/post/{id}', [PostController::class, "show"]);
+
+Route::post("/posts", [PostController::class, "store"]);
