a14bcd6994315d2f36ef571ed031a787c3c6d19b
Alex Gonzalez <galex2003@icloud.com>
2024-09-07 19:14:11 -0400

    Added route for all posts

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -12,7 +12,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+        return response()->json($posts);
     }
 
     /**
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/post/{id}', [PostController::class, "show"]);
-
 Route::post("/posts", [PostController::class, "store"]);
+Route::get("/posts", [PostController::class,"index"]);
