cb6cf6091abe9e3ee2df0886a45250801da5e93b
Alex Gonzalez <galex2003@icloud.com>
2024-09-13 13:54:19 -0400

    foundations of user auth and model for user

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -3,6 +3,9 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Laravel\Sanctum\Http\Middleware\CheckAbilities;
+use Laravel\Sanctum\Http\Middleware\CheckForAnyAbility;
+ 
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,4 +18,10 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })->create()
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'abilities' => CheckAbilities::class,
+            'ability' => CheckForAnyAbility::class,
+        ]);
+    });
