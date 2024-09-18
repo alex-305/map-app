1168275ca7d838a2c1b9c04ad64e712ec9b85bc8
Alex Gonzalez <galex2003@icloud.com>
2024-09-18 15:24:42 -0400

    CRUD functionality for posts

---
+++ backend/app/Http/Controllers/PostController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::all();
+
+        return response()->json($posts);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'author_id' => 'required|integer',
+            'content' => 'required|string',
+            'latitude' => 'required|numeric',
+            'longitude' => 'required|numeric'
+        ]);
+
+        $post = Post::create($validatedData);
+
+        return response()->json($post, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $post = Post::findOrFail($id);
+
+        return response()->json($post, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $post = Post::findOrFail($id);
+
+        $validatedData = $request->validate([
+            'author_id' => 'required|integer',
+            'content' => 'required|string',
+            'latitude' => 'required|numeric',
+            'longitude' => 'required|numeric'
+        ]);
+
+        $post->update($validatedData);
+
+        return response()->json($post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+
+        return response()->json(null, 204);
+    }
+}
---
+++ backend/database/migrations/2024_09_18_174715_create_posts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger(column: 'author_id');
+            $table->string('content');
+            $table->integer('like_count')->default(0);
+            $table->decimal('latitude')->default(0.0);
+            $table->decimal('longitude')->default(0.0);
+            $table->timestamps();
+
+            $table->foreign('author_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
