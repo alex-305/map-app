1a5520d1a7f18eee448b3103760eddef74627bd6
sam-morgan <andersonhenderson45@gmail.com>
2024-09-23 13:15:27 -0400

    added Posts model, table, etc
    * new endpoint /posts
        * GET /posts or /posts/{id}
        * GET /posts
        * PUT /posts/{id}
        * DELETE /posts/{id}
    * new migration and controller too

---
+++ backend/app/Http/Controllers/PostController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::all();
+
+        return response()->json($posts, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'author_id' => 'require|integer',
+            'content' => 'required|string',
+            'latitude' => 'required|numeric',
+            'longitude' => 'required|numeric'
+        ]);
+
+        $post = Post::create($validatedData);
+
+        return response()->json($post, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $post = Post::findOrFail($id);
+
+        return response()->json($post, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $post = Post::findOrFail($id);
+
+        $validatedData = $request->validate([
+            'author_id' => 'require|integer',
+            'content' => 'required|string',
+            'latitude' => 'required|numeric',
+            'longitude' => 'required|numeric'
+        ]);
+
+        $post->update($validatedData);
+
+        return response()->json($post, 204);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $post = Post::findOrFail($id);
+
+        $post->delete();
+
+        return response()->json(null, 204);
+    }
+}
