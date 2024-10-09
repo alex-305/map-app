14926ea89f342399d9fe56633ce69ae27d5acef8
Alex Gonzalez <galex2003@icloud.com>
2024-10-09 12:30:20 -0400

    refactored http requests to be more generic

--- resources/js/scripts/http.ts
+++ resources/js/scripts/http.ts
@@ -1,49 +1,46 @@
-export async function post(url: string, payload?: any, headers?: any): Promise<any> {
+export async function post(url: string, payload?: any, headers?: any): Promise<any> { return http_request('POST', url, headers, payload)}
+
+export async function del(url: string, payload?: any, headers?: any): Promise<any> { return http_request('DELETE', url, headers) }
+
+export async function get(url: string, headers?: any): Promise<any> { return http_request('GET', url, headers) }
+
+
+async function http_request(req_type: string, url: string, headers_passed?: any, payload?: any): Promise<any> {
   // we have to manually put the csrf token in the request
   const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
-  const res = await fetch(url, {
-    method: 'POST',
-    headers: {
-      ...headers,
-      'X-CSRF-TOKEN': csrfToken,
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(payload || {})
-  })
+  const headers = {
+    ...headers_passed,
+    'X-CSRF-TOKEN': csrfToken,
+    'Accept': 'application/json'
+  }
+
+  if(req_type==='POST') {
+    headers['Content-Type'] = 'application/json'
+  }
+
+  const requestOptions: RequestInit = {
+    method: req_type,
+    headers: headers
+  }
+
+  if(payload) {
+    requestOptions['body'] = payload
+  }
+
+  const res = await fetch(url, requestOptions)
+
+
   const data = await res.json()
 
   let errors: {
     message?: string
   } | undefined
 
-  if (!res.ok)
-    errors = { message: data.message }
-
-  return { data, errors }
-}
-
-export async function get(url: string, headers?: any): Promise<any> {
-  // we have to manually put the csrf token in the request
-  const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-
-  const res = await fetch(url, {
-    method: 'get',
-    headers: {
-      ...headers,
-      'X-CSRF-TOKEN': csrfToken,
-      'Accept': 'application/json',
-    },
-  })
-  const data = await res.json()
-
-  let errors: {
-    message?: string
-  } | undefined
+  const status = res.status
 
   if (!res.ok)
     errors = { message: data.message }
 
-  return { data, errors }
+  return { data, errors, status }
 }
