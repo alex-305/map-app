17750067740723bd4d9d317df851b556a75980bf
Alex Gonzalez <galex2003@icloud.com>
2024-10-15 16:37:13 -0400

    Added default layout to whole app

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -3,10 +3,17 @@ import "./index.css";
 import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
+import RootLayout from './layouts/RootLayout';
 
 createInertiaApp({
 
-  resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
+  resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')).then((module) => {
+    const page = module.default
+
+    page.layout = page.layout || ((page) => <RootLayout>{page}</RootLayout>)
+    
+    return page
+  }),
   setup({ el, App, props }) {
       const root = createRoot(el);
 
