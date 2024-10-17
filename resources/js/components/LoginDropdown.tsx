1403d90987697a34ba76e8f15851bbd1520e8dcf
Alex Gonzalez <galex2003@icloud.com>
2024-10-17 15:08:32 -0400

    Added registration date

--- resources/js/components/LoginDropdown.tsx
+++ resources/js/components/LoginDropdown.tsx
@@ -5,6 +5,7 @@ import { RegisterDialog } from "./RegisterDialog"
 import { Separator } from "./ui/separator"
 import { useUserInfo } from "./UserInfoContext"
 import useAuth from "@/scripts/useAuth"
+import { formatDate } from "@/scripts/formatDate"
 
 export default function LoginDropdown() {
     const { loggedIn, user } = useUserInfo()
@@ -20,9 +21,13 @@ export default function LoginDropdown() {
                 <Button className='rounded-none' variant="ghost">Account</Button>
             </PopoverTrigger>
             <PopoverContent className="w-auto flex flex-col gap-4">
-                <div>Hey <span className="font-bold">{
-                    user?.username ?? 
-                    <span className="italic">Anonymous User</span>}</span>!
+                <div className="flex flex-col items-center justify-center">
+                    <div>
+                        Hey <span className="font-bold">{
+                        user?.username ?? 
+                        <span className="italic">Anonymous User</span>}</span>!
+                    </div>
+                    <div>Member since {formatDate(user?.created_at) ?? "Unknown"}</div>
                 </div>
                 <div className="flex">
                     <Button variant="link">Settings</Button>
