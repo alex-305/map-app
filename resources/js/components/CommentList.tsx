42929132a88a27341c157958b512ad536ee6e31b
Alex Gonzalez <galex2003@icloud.com>
2024-10-17 17:40:37 -0400

    Added separator for comments

--- resources/js/components/CommentList.tsx
+++ resources/js/components/CommentList.tsx
@@ -2,6 +2,8 @@ import { get } from "@/scripts/http";
 import { useEffect, useState } from "react";
 import { Comment } from "@/types/Comment";
 import { ScrollArea } from "./ui/scroll-area";
+import { formatDate } from "@/scripts/formatDate";
+import { Separator } from "./ui/separator";
 
 export default function CommentList({ authorId, postId }) {
   const [comments, setComments] = useState<Array<Comment>>(null)
@@ -20,7 +22,7 @@ export default function CommentList({ authorId, postId }) {
       <div className="flex flex-col gap-4">
         {comments?.length>0 && Array.isArray(comments) && comments.map((c, i) => {
           return (
-            <div className="flex flex-col" key={i}>
+            <div className="flex flex-col py-0" key={i}>
               <div className="flex justify-between w-[96%]">
                 <span>{c.content}</span>
                 {c.author_id === authorId && (
@@ -29,7 +31,8 @@ export default function CommentList({ authorId, postId }) {
               </div>
               <span className="text-xs text-gray-500">posted by <span className="font-semibold">
                 {c.username}
-                </span> on {(new Date(c.created_at)).toLocaleDateString('en-US')}</span>
+                </span> on {formatDate(c.created_at)}</span>
+                {i<comments.length-1 && <Separator className="mt-4"/>}
             </div>
           )
         })}
